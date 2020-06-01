ddd7406a3592a76866ca9f50d0a7bcb3f483179c
Sukriti Bhardwaj <sukriti@Sukritis-MacBook-Air.local>
2020-06-01 12:37:00 -0400

    commit on 'master'

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -15,19 +15,19 @@ class Business extends React.Component {
         return (
             <div className="Business">
             <div className="image-container">
-            <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+            <img src= {business.imageSrc} alt=''/>
             </div>
             <h2>MarginOtto Pizzeria</h2>
             <div className="Business-information">
                 <div className="Business-address">
-                <p>1010 Paddington Way</p>
+                <p>{business.address}</p>
                 <p>Bordertown</p>
                 <p>NY 10101</p>
                 </div>
                 <div className="Business-reviews">
                 <h3>ITALIAN</h3>
-                <h3 className="rating">4.5 stars}</h3>
-                <p>90 reviews</p>
+                <h3 className="rating">{business.rating}</h3>
+                <p>{business.reviewCount + 'reviews'}</p>
                 </div>
             </div>
             </div>
