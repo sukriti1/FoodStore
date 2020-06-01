1ed74b80f49b22b413864b28a35eb410608d58a0
Sukriti Bhardwaj <sukriti@Sukritis-MacBook-Air.local>
2020-06-01 14:26:34 -0400

    commit

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -12,27 +12,27 @@ const business = {
   reviewCount: 90
 }
 class Business extends React.Component {
-    render(){
-        return (
-            <div className="Business">
-            <div className="image-container">
-            <img src= {business.imageSrc} alt=''/>
+    render() {
+      return (
+        <div className="Business">
+          <div className="image-container">
+            <img src={business.imageSrc} alt=''/>
+          </div>
+          <h2>{business.name}</h2>
+          <div className="Business-information">
+            <div className="Business-address">
+              <p>{business.address}</p>
+              <p>{business.city}</p>
+              <p>{`${business.state} ${business.zipCode}`}</p>
             </div>
-            <h2>MarginOtto Pizzeria</h2>
-            <div className="Business-information">
-                <div className="Business-address">
-                <p>{business.address}</p>
-                <p>Bordertown</p>
-                <p>NY 10101</p>
-                </div>
-                <div className="Business-reviews">
-                <h3>ITALIAN</h3>
-                <h3 className="rating">{business.rating}</h3>
-                <p>{business.reviewCount + 'reviews'}</p>
-                </div>
+            <div className="Business-reviews">
+              <h3>{business.category.toUpperCase()}</h3>
+              <h3 className="rating">{`${business.rating} stars`}</h3>
+              <p>{`${business.reviewCount} reviews`}</p>
             </div>
-            </div>
-        )
+          </div>
+        </div>
+      );
     }
-}
+  }
 export default Business;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,33 +1,38 @@
 import React from 'react';
-import './ SearchBar.css'
+import './SearchBar.css';
+
 const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-}
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+};
+
 class SearchBar extends React.Component {
-    renderSortByOptions(){
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key = {sortByOptionValue}>{sortByOption}</li>
-        });
-    }
-    render(){
-        <div className="SearchBar">
+  renderSortByOptions() {
+    return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
         <div className="SearchBar-sort-options">
-        <ul>
-              {this.renderSortByOptions()}      
-        </ul>
+          <ul>
+            {this.renderSortByOptions()}
+          </ul>
         </div>
         <div className="SearchBar-fields">
-            <input placeholder="Search Businesses" />
-            <input placeholder="Where?" />
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-            <a>Let's Go</a>
+          <a>Let's Go</a>
         </div>
-        </div>
-
-    }
+      </div>
+    );
+  }
 }
+
 export default SearchBar;
